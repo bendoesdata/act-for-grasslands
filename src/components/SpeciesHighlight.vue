08d0246e0ce02caa59a45a3da2065f299f96fa2d
Ben Cooley <benjamincooley94@gmail.com>
2023-09-12 12:36:20 -0400

    optimize

--- src/components/SpeciesHighlight.vue
+++ src/components/SpeciesHighlight.vue
@@ -1,4 +1,7 @@
 <template>
+  <div>
+
+  
     <div class="container" id="species-highlight-description">
       <div class="left">
         <img :src="'../'+imageUrl" alt="Left Image" :style="{ width: isMobile ? '100%' : 'auto' }">
@@ -6,9 +9,17 @@
       <div class="right">
         <h2>{{ title }}</h2>
         <div class="species-description" v-html="description"></div>
-        
+        <br>
+        <p>By changing nothing, nothing changes.
+      <a href="#" style="text-decoration: underline;">Click here</a> to call on Congress to
+      restore disappearing grasslands by supporting a North American Grasslands Conservation Act.
+      </p>
       </div>
     </div>
+    <div class="species-map-placeholder">
+      species specific map coming soon
+    </div>
+  </div>
   </template>
   
   <script>
@@ -36,6 +47,16 @@
   </script>
   
   <style scoped>
+
+  .species-map-placeholder {
+    height: 300px;
+    background-color: #475026;
+    color: #EFEAD4;
+    display: flex;
+    margin-top: 20px;
+    justify-content: center;
+    align-items: center;
+  }
   .container {
     display: flex;
     flex-wrap: wrap;
