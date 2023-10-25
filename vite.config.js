60f5881c2198e8e388c46de8f374d399d95b3b76
Ben Cooley <benjamincooley94@gmail.com>
2023-10-25 11:22:57 -0400

    dist build

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import { fileURLToPath, URL } from 'node:url'
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/act-for-grasslands/',
   plugins: [vue()],
-  build: {
-    outDir: 'docs'
-  },
-  // resolve: {
-  //   alias: {
-  //     '@': fileURLToPath(new URL('./src', import.meta.url))
-  //   }
-  // }
 })
