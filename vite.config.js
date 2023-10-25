ba09c1757fdcbf750f21084c9dff7291ab6a70b3
Ben Cooley <benjamincooley94@gmail.com>
2023-10-25 10:23:01 -0400

    test docs build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { fileURLToPath, URL } from 'node:url'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/act-for-grasslands/' : '/',
   plugins: [vue()],
+  build: {
+    outDir: 'docs'
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
