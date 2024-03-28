cef3b91c43fd136f81a2f0badd5ab4a876b1770e
bendoesdata <benjamincooley94@gmail.com>
2024-03-28 16:14:25 -0400

    add citation text

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,6 +2,7 @@
     <footer class="footer">
       <div class="footer-content">
         <p class="footer-text">Map for Grasslands was co-created by the <a href="https://www.birds.cornell.edu/conservation-media/" aria-label="link text - new window" target="_blank">Center for Conservation Media</a> at the Cornell Lab of Ornithology and the coalition members of <a href="https://actforgrasslands.org/" aria-label="link text - new window" target="_blank">Act for Grasslands</a>. ©2024</p>
+        <p style="font-style: italic; font-size: 14px; text-align: center">Suggested citation: Cornell Lab of Ornithology and Act for Grasslands (2024). Map for Grasslands. https://mapforgrasslands.org</p>
       </div>
     </footer>
   </template>
@@ -16,7 +17,7 @@
   .footer {
     background-color: #475026;
     color: #efead4;
-    height: 180px;
+    height: 270px;
     width: 100%;
     display: flex;
     justify-content: center;
